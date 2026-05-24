ab75a03a7e27f09a5a8e15c0c8066aa366e4a925
Don Kendall <dkendall@ledoweb.com>
2026-05-24 12:25:29 -0400

    [MIG] l10n_us_gaap: Migration to 19.0

--- l10n_us_gaap/__manifest__.py
+++ l10n_us_gaap/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "United States Sample GAAP Chart of Accounts",
-    "version": "17.0.1.0.0",
+    "version": "19.0.1.0.0",
     "development_status": "Mature",
     "category": "Accounting/Localizations/Account Charts",
     "countries": ["us"],
